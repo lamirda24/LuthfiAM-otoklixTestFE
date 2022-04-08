1cdf06d19af24280beabe27ea78e6e352a27ab64
LuthfiAM <luthfianugerah@gmail.com>
2022-04-08 11:29:24 +0700

    layout

---
+++ src/components/Content/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./Dashboard/Dashboard";
+
+const Content = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Content;
